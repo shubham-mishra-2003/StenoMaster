2998b1140aa29f384137ae1e0af2314032c17399
Mayank Kumar Grade <mayankkumargrade@gmail.com>
2025-06-23 15:38:30 +0530

    fix: remove redundant suppressHydrationWarning from body tag in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
+      <body suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
