663d625bec566653afa2c398ea4293e3f529e9df
shubhammishra3070 <shubhammishra3070@gmail.com>
2025-09-06 06:04:58 +0000

    Resolved build issues

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  devIndicators: {
-    allowedDevOrigins: [
-      "https://3000-firebase-stenomaster-1757135644702.cluster-mwsteha33jfdowtvzffztbjcj6.cloudworkstations.dev",
-    ],
-  },
 };
 
 export default nextConfig;
